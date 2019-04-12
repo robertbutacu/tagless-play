385f62e61318254b6d612fc69b7ff201919df71f
robertbutacu <17516693+robertbutacu@users.noreply.github.com>
2019-04-12 20:16:44 +0100

    experimenting more for the type checks

--- src/main/scala/experiments/Experiments.scala
+++ src/main/scala/experiments/Experiments.scala
@@ -3,41 +3,49 @@ package experiments
 import cats.{Monad, ~>}
 import generic.action.filter.{GenericActionFilter, GenericActionRefiner}
 import play.api.mvc._
-import cats.instances.all._
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.{ExecutionContext, ExecutionContextExecutor, Future}
 import scala.language.higherKinds
 import generic.action.builder.GenericActionBuilder.GenericActionBuilder
 import play.api.libs.json.Json
 
-object Experiments {
+object Experiments extends App {
+
+  implicit val ec: ExecutionContextExecutor = ExecutionContext.global
 
   case class RequestWithProviderId[A](providerId: String, request: Request[A]) extends WrappedRequest[A](request)
 
-  class RequestFiltered[F[_] : Monad](implicit val executionContext: ExecutionContext,
-                                      val transformer: F ~> Future) extends GenericActionFilter[F, Request] {
+  class RequestFiltered[F[_] : Monad]()(implicit val transformer: F ~> Future) extends GenericActionFilter[F, Request] {
+
     override def genericFilter[A](request: Request[A]): F[Option[Result]] =
       implicitly[Monad[F]].pure(None)
+
+    override protected def executionContext: ExecutionContext = ec
   }
 
-  class ExtraRequest[F[+_]](implicit val executionContext: ExecutionContext, val transformer: F ~> Future, M: Monad[F])
+  class ExtraRequest[F[+_]]()(implicit val transformer: F ~> Future, M: Monad[F])
     extends GenericActionRefiner[F, Request, RequestWithProviderId] {
+
     override def genericRefine[A](request: Request[A]): F[Either[Result, RequestWithProviderId[A]]] = {
       M.pure(Right(RequestWithProviderId("some-provider-id", request)))
     }
+
+    override protected def executionContext: ExecutionContext = ec
   }
 
   class ComposedActions[F[+_]](
                        requestFiltered: RequestFiltered[F],
                        extraRequest: ExtraRequest[F],
-                       someService: SomeService[F],
                        cc: ControllerComponents
                        )(implicit M: Monad[F], transformer: F ~> Future) extends AbstractController(cc) {
     def filtered: ActionBuilder[RequestWithProviderId, AnyContent] = Action andThen requestFiltered andThen extraRequest
-
-    filtered.genericAsync(implicit request => M.pure(Ok(Json.obj())))
   }
 
-  class SomeService[F[_]] {
+  class Controller[F[+_]](composedActions: ComposedActions[F],
+                          someService: SomeService[F],
+                         cc: ControllerComponents)(implicit M: Monad[F], transformer: F ~> Future) extends AbstractController(cc) {
 
+    def someAction: Action[AnyContent] = composedActions.filtered.genericAsync(implicit request => M.pure(Ok(Json.obj())))
   }
+
+  class SomeService[F[_]]()
 }
